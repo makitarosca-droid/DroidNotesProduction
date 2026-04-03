08f6ce400bf88f8cd0fd9fe602f5c38965668444
Rosca Mihai Marian <m.rosca@mmrmakita.eu>
2026-04-03 12:22:20 +0300

    update

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/DroidNotesProduction/sw.js', { scope: '/DroidNotesProduction/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
